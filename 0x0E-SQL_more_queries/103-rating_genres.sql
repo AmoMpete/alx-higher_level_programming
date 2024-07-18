30d5c0cb22fffd18dd9de2278b06a02dfecf79ac
AmoMpete <amompete@gmail.com>
2024-07-18 08:07:12 -0700

    SQL - More queries

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,11 @@
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating
+-- Each record should display: tv_genres.name - rating sum
+-- Results must be sorted in descending order by their rating
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
+SELECT tv_genres.name, SUM(tv_show_ratings.rating) AS total_rating
+FROM tv_genres
+JOIN tv_show_ratings ON tv_genres.id = tv_show_ratings.genre_id
+GROUP BY tv_genres.name
+ORDER BY total_rating DESC;
---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,12 @@
+-- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+-- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON id=tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title;
